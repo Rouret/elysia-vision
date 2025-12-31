aaf05f997d1adbcfa0bf6bdc226e95b95e46fd31
Lucas ROURET <lucas.rouretperso@gmail.com>
2025-12-31 11:30:47 +0100

    deadcode: remove useless path filter on path

--- packages/vision/src/constants.ts
+++ packages/vision/src/constants.ts
@@ -1,4 +1,3 @@
 export const VISION_CALL_EVENT_NAME = "vision-call";
 export const VISION_REQUEST_ID_HEADER = "vision-request-id";
-export const VISION_PATHS = ["/vision", "/.well-known"];
 export const HEADERS_TO_REMOVE = [VISION_REQUEST_ID_HEADER];
